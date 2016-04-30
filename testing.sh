840802bfbde6f964c8c280f7c5d4460b7d4bc568
Emory Merryman <emory.merryman@gmail.com>
2016-04-30 18:40:39 +0000

    added stuff

--- testing.sh
+++ testing.sh
@@ -2,6 +2,7 @@
 
 time docker build -t ninthgrimmercury/easternmoose . &&
     time docker build -t freakygamma/easternmoose test &&
+    docker run --interactive --tty freakygamma/easternmoose is-enabled dnf-makecache.timer &&
     [[ "disabled" == $(docker run --interactive --tty freakygamma/easternmoose is-enabled dnf-makecache.timer) ]] &&
     echo PASSED &&
     docker rmi ninthgrimmercury/easternmoose &&
