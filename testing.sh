68b913e9d9c2c2b79f9fbd2283601a2f7e9423a7
Emory Merryman <emory.merryman@gmail.com>
2016-04-30 18:09:13 +0000

    wtf

--- testing.sh
+++ testing.sh
@@ -2,13 +2,8 @@
 
 time docker build -t ninthgrimmercury/easternmoose . &&
     time docker build -t freakygamma/easternmoose test &&
-    docker run --interactive --tty freakygamma/easternmoose echo hi &&    
-    echo BEFORE &&
-    docker run --interactive --tty freakygamma/easternmoose systemctl is-enabled dnf-makecache.timer &&
-    echo AFTER &&
-    X=$(docker run --interactive --tty freakygamma/easternmoose systemctl is-enabled dnf-makecache.timer) &&
-    echo X=${X} &&
     [[ "disabled" == $(docker run --interactive --tty freakygamma/easternmoose systemctl is-enabled dnf-makecache.timer) ]] &&
+    echo PASSED &&
     docker rmi ninthgrimmercury/easternmoose &&
     docker rmi freakygamma/easternmoose &&
     true
