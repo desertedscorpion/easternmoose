0f00ede28c40ad849e018bd6e8a8be57f6635de6
Emory Merryman <emory.merryman@gmail.com>
2016-04-30 17:17:44 +0000

    this test should also fail

--- testing.sh
+++ testing.sh
@@ -2,7 +2,7 @@
 
 docker build -t ninthgrimmercury/easternmoose . &&
     docker build -t freakygamma/easternmoose test &&
-    docker run --interactive --tty freakygamma/easternmoose test rm /tmp/no/such/file &&
+    docker run --interactive --tty freakygamma/easternmoose systemctl [[ "wrong" == is-enabled dnf-makecache.timer ]] &&
     docker rmi ninthgrimmercury/easternmoose &&
     docker rmi freakygamma/easternmoose &&
     true
