5f350c0839dc4ad99383e72122a672e0ad1e6031
Emory Merryman <emory.merryman@gmail.com>
2016-04-30 17:15:16 +0000

    removing images and this test should fail

--- testing.sh
+++ testing.sh
@@ -2,5 +2,7 @@
 
 docker build -t ninthgrimmercury/easternmoose . &&
     docker build -t freakygamma/easternmoose test &&
-    docker run --interactive --tty freakygamma/easternmoose test false &&
+    docker run --interactive --tty freakygamma/easternmoose test rm /tmp/no/such/file &&
+    docker rmi ninthgrimmercury/easternmoose &&
+    docker rmi freakygamma/easternmoose &&
     true
