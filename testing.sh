738bb9753adedd484b2669724a961080d2b03a2e
Emory Merryman <emory.merryman@gmail.com>
2016-05-01 17:31:23 +0000

    grep it

--- testing.sh
+++ testing.sh
@@ -2,9 +2,7 @@
 
 time docker build -t ninthgrimmercury/easternmoose . &&
     time docker build -t freakygamma/easternmoose test &&
-    DNFUPDATEFILE=$(mktemp) &&
-    docker run --interactive --tty freakygamma/easternmoose dnf update --assumeyes > ${DNFUPDATEFILE} 2>&1 &&
-    echo ${DNFUPDATEFILE} &&
+    docker run --interactive --tty freakygamma/easternmoose dnf update --assumeyes | grep "^Last metadata expiration check: \d*:\d*:\d* ago on ... ...  \d* \d*:\d*:\d* \d*\$" &&
     if [[ "disabled" == "$(docker run --interactive freakygamma/easternmoose systemctl is-enabled dnf-makecache.timer)" ]]
     then
 	echo dnf-makecache.timer is disabled. &&
