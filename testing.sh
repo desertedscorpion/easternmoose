9f34d39385e115b5b94ce8811a60ae12d701a396
Emory Merryman <emory.merryman@gmail.com>
2016-05-01 14:15:31 +0000

    this will probably fix the problem that systemctl is-enabled exits with 1

--- testing.sh
+++ testing.sh
@@ -3,8 +3,14 @@
 time docker build -t ninthgrimmercury/easternmoose . &&
     time docker build -t freakygamma/easternmoose test &&
     docker run --interactive --tty freakygamma/easternmoose is-enabled dnf-makecache.timer &&
-    [[ "disabled" == $(docker run --interactive --tty freakygamma/easternmoose systemctl is-enabled dnf-makecache.timer) ]]
-    echo PASSED &&
+    if [[ "disabled" == $(docker run --interactive --tty freakygamma/easternmoose systemctl is-enabled dnf-makecache.timer) ]]
+    then
+	echo dnf-makecache.timer is disabled &&
+	    true
+    else
+	echo dnf-makecache.timer is not disabled &&
+	    exit 64
+    fi &&
     docker rmi ninthgrimmercury/easternmoose &&
     docker rmi freakygamma/easternmoose &&
     true
