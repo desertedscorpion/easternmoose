bf24b6d75ef531f3b7b036ebc834ab98843bac83
Emory Merryman <emory.merryman@gmail.com>
2016-05-01 17:46:57 +0000

    comment on failure

--- testing.sh
+++ testing.sh
@@ -2,14 +2,22 @@
 
 time docker build -t ninthgrimmercury/easternmoose . &&
     time docker build -t freakygamma/easternmoose test &&
-    docker run --interactive --tty freakygamma/easternmoose dnf update --assumeyes | grep "^Last metadata expiration check: 0:00:\d\d ago on ... ...  \d \d*:\d*:\d* \d\d\d\d.\$" &&
+    if docker run --interactive --tty freakygamma/easternmoose dnf update --assumeyes | grep "^Last metadata expiration check: 0:00:"
+    then
+	echo dnf was updated within the last hour &&
+	    true
+    else
+	echo dnf was not updated within the last hour &&
+	    exit 64 &&
+	    true
+    fi &&
     if [[ "disabled" == "$(docker run --interactive freakygamma/easternmoose systemctl is-enabled dnf-makecache.timer)" ]]
     then
 	echo dnf-makecache.timer is disabled. &&
 	    true
     else
 	echo dnf-makecache.timer is not disabled. &&
-	    exit 64 &&
+	    exit 65 &&
 	    true
     fi &&
     docker rmi --force ninthgrimmercury/easternmoose &&
